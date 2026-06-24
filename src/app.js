fd7126236c9de3ce2385a4bdef3fe7fbf2f251e9
Muhammed Sudheer A.S <muhammedsudheer948@gmail.com>
2026-06-24 20:11:38 +0530

    exploring get, post and delete

--- src/app.js
+++ src/app.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Akshay", lastName: "Saini" });
+});
+
+app.post("/user", (req, res) => {
+  console.log("Save Data to the database");
+  res.send("Data successfully saved to the database!");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Deleted successfully");
+});
+
 app.use("/hello", (req, res) => {
   res.send("Hello hello hello hehehehehe");
 });
