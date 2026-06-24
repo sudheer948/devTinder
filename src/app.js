48880bafca1f87c8f356055ae9f1d959e68bc1fe
Muhammed Sudheer A.S <muhammedsudheer948@gmail.com>
2026-06-24 18:27:07 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/hello", (req, res) => {
+  res.send("Hello hello hello hehehehehe");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Hello from the server!");
+});
+
+app.listen(7777, () => {
+  console.log("Server is successfully listening on port 7777...");
+});
